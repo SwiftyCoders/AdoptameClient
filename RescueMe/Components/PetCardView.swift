adce5664951562e67327f9992d1d449ab14eb9c6
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2025-02-05 15:22:36 +0100

    [Feature]  Create PetCardView

---
+++ RescueMe/Components/PetCardView.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct PetCardView: View {
+    
+    //MARK: PetModel
+    let imagePet: Image
+    
+    var body: some View {
+        
+        VStack (alignment: .leading){
+            ZStack {
+                Color.gray.opacity(0.2)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .frame(width: 300, height: 300)
+                
+                
+                ZStack(alignment: .topTrailing) {
+                    Image(.dogImageMock)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                    
+                    HeartButton {}
+                        .padding()
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Pet name")
+                    .font(.title)
+                    .bold()
+                
+                HStack(spacing: 8) {
+                    Image(systemName: "mappin.and.ellipse")
+                        .foregroundColor(.orange)
+                    Text("1.8 km ")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    Circle()
+                        .fill(Color.gray)
+                        .frame(width: 2, height: 2)
+                    Text("Shelter")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    PetCardView(imagePet: Image(.dogImageMock))
+}
