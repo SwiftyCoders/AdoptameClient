b533186334af6e39ad3fe5240ea93071627d4587
Alam Alonzo <alonzo.alam@gmail.com>
2025-02-10 10:09:51 -0400

    [Fix] Clean code

--- RescueMe/Components/PetsGrid.swift
+++ RescueMe/Components/PetsGrid.swift
@@ -1,15 +1,9 @@
-//
-//  PetsGrid.swift
-//  RescueMe
-//
-//  Created by Alam Alonzo on 9/2/25.
-//
-
 import SwiftUI
 
 struct PetsGrid: View {
     let shelter: Shelter
     let columns = Array(repeating: GridItem(.flexible(), spacing: 16), count: 2)
+    
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 16) {
