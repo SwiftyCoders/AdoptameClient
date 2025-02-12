890e1a50012105370f5acad6f036c9199ebe761c
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2025-02-12 20:08:54 +0100

    [Feature] Create ShelterPreviewCell

--- RescueMe/Components/ShelterPreviewCell.swift
+++ RescueMe/Components/ShelterPreviewCell.swift
@@ -1,18 +1,59 @@
-//
-//  ShelterPreviewCell.swift
-//  RescueMe
-//
-//  Created by Laura Isabel Rojas Bustamante on 12/02/25.
-//
-
 import SwiftUI
 
 struct ShelterPreviewCell: View {
+    
+    var shelter: Shelter
+    var onTapGesture: () -> Void
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button(action: onTapGesture) {
+            HStack(spacing: 16) {
+                shelterImage
+                shelterInformation
+                chevronImage
+            }
+            .frame(maxWidth: .infinity)
+        }
+        .foregroundStyle(.primary)
+    }
+    
+    private var chevronImage: some View {
+        Image(systemName: "chevron.right.circle.fill")
+            .font(.title)
+            .foregroundStyle(.primaryOrange)
+    }
+    
+    private var shelterImage: some View {
+        Image(.hiro1)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 80)
+            .clipShape(Circle())
+            .padding()
+            .accessibilityLabel("Shelter image of \(shelter.name)")
+    }
+    
+    private var shelterInformation: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(shelter.name)
+                .font(.title2)
+                .bold()
+                .accessibilityLabel("Shelter name: \(shelter.name)")
+                .accessibilityAddTraits(.isHeader)
+                .accessibilitySortPriority(1)
+            
+            Text(shelter.formattedAddress)
+                .font(.callout)
+                .foregroundStyle(.secondary)
+                .dynamicTypeSize(.medium ... .accessibility3)
+        }
     }
 }
 
+
 #Preview {
-    ShelterPreviewCell()
+    VStack {
+        ShelterPreviewCell(shelter: .previewShelter, onTapGesture: {})
+    }
 }
