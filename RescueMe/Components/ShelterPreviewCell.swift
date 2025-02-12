4f2a611f830c0a903ed9a41356176f7d67bd8b32
Laura Rojas Bustamante <151399958+Laura-irb18@users.noreply.github.com>
2025-02-12 20:06:34 +0100

    [Feature] Create ShelterPreviewCell

---
+++ RescueMe/Components/ShelterPreviewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ShelterPreviewCell.swift
+//  RescueMe
+//
+//  Created by Laura Isabel Rojas Bustamante on 12/02/25.
+//
+
+import SwiftUI
+
+struct ShelterPreviewCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShelterPreviewCell()
+}
