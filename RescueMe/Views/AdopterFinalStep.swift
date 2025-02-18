8db3b7b92e56b6cb063c7041c19fb849a4a507e9
Alam Alonzo <alonzo.alam@gmail.com>
2025-02-17 21:58:40 -0400

    [Feature] View adopterFinal Steps. put your Name

--- RescueMe/Views/AdopterFinalStep.swift
+++ RescueMe/Views/AdopterFinalStep.swift
@@ -3,13 +3,24 @@ import SwiftUI
 struct AdopterFinalStep: View {
     @State private var progress = 0.25
     @State private var userName = ""
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Final Steps!")
-                .font(.largeTitle)
-            Text("we're almost there! fill in your personal details to create a profile and start your journey towards a furry friendships.")
-                .lineLimit(3)
-            ImagePicker()
+        VStack(alignment: .leading, spacing: 40) {
+            VStack(alignment: .leading, spacing: 6){
+                Text("Final Steps!")
+                    .bold()
+                    .font(.largeTitle)
+                    .accessibilityAddTraits(.isHeader)
+                Text("we're almost there! fill in your personal details to create a profile and start your journey towards a furry friendships.")
+                    .accessibilityLabel("Estamos casi listos. Ingresa tus datos personales para crear tu perfil y comenzar tu camino hacia nuevas amistades.")
+                    .lineLimit(3)
+            }
+            HStack {
+                Spacer()
+                ImagePicker(size: 180)
+                    .accessibilityLabel("Selecciona una imagen de perfil")
+                Spacer()
+            }
             CustomTextField(
                 input: $userName,
                 label: "User Name",
@@ -22,10 +33,13 @@ struct AdopterFinalStep: View {
                         return nil
                     }
                 }
+                .accessibilityElement(children: .combine)
             Spacer()
             Button("Finish") { }
                 .buttonPrimaryStyle()
                 .bold()
+                .accessibilityLabel("Finalizar")
+                .accessibilityHint("Completa el proceso de creación de tu perfil")
         }
         .safeAreaInset(edge: .top) {
             HStack {
@@ -34,6 +48,9 @@ struct AdopterFinalStep: View {
                 Text(" \(Int(progress * 4)) / 4")
                     .padding(.leading)
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Progreso")
+            .accessibilityValue("\(Int(progress * 4)) de 4 completados")
         }
         .safeAreaPadding(.horizontal)
     }
